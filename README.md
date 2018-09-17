fc7e27f4dd254ef34ea776385928d1be88f8376a
顾重 <gucx@dxy.cn>
2018-09-18 02:03:26 +0800

    feat: save

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# [dxd-we](https://github.com/guchongxi/dxd-we-cli)
+
+丁香医生前端微信小程序开发脚手架
+
+## License
+
+This project is licensed under the terms of the MIT license
---
+++ bin/index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const updateNotifier = require('update-notifier');
+const { yellow } = require('chalk');
+const pkg = require('../package.json');
+const { PKG_DESCRIPTION } = require('../lib/constant');
+const { enhanceErrorMessages } = require('../lib/utils');
+
+// 版本更新提示
+// 默认一天提醒一次
+updateNotifier({ pkg })
+  .notify({
+    // 在命令执行时直接提示
+    defer: false
+  });
+
+// 工具版本
+program
+  .version(pkg.version)
+  .name('dxd-we')
+  .description(PKG_DESCRIPTION)
+  .usage('<command> [options]');
+
+program
+  .command('create <app-name>')
+  .alias('c')
+  .description('create a new project powered by dxd-we')
+  .option('-f, --force', 'Overwrite target directory if it exists')
+  .action((appName, options) => {
+    require('../lib/create')(appName, options);
+  });
+
+enhanceErrorMessages('missingArgument', argName => {
+  return `Missing required argument ${yellow(`<${argName}>`)}.`;
+});
+
+enhanceErrorMessages('unknownOption', optionName => {
+  return `Unknown option ${yellow(optionName)}.`;
+});
+
+enhanceErrorMessages('optionMissingArgument', (option, flag) => {
+  return `Missing required argument for option ${yellow(option.flags)}${flag ? `, got ${yellow(flag)}` : ''}`;
+});
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
